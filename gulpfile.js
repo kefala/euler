350fd9264c4c4fb7e55d95fe6429e99696658b26
kefala <martin.g.lobo@gmail.com>
2016-05-03 17:49:00 -0300

    Init

---
+++ gulpfile.js
@@ -0,0 +1,214 @@
+var paths = {
+  js : {
+
+    dev : [
+    ],
+
+    vendor : [
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/underscore/underscore-min.js',
+    'node_modules/backbone/backbone-min.js',
+    'node_modules/materialize/dist/js/materialize.min.js'
+    ]
+  },
+
+  css: {
+    dev: [
+    ],
+    vendor: [
+    'node_modules/materialize/dist/css/materialize.min.css',
+    ]
+  },
+
+  html: {
+    directives: [
+    ],
+    views: [
+    ]
+  },
+
+  'fonts': {
+      fonts: []
+  }
+
+};
+
+//genericos
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
+var order = require("gulp-order");
+//javascript
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+//sass
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+//html
+var rename = require('gulp-rename');
+var minifyHTML = require('gulp-minify-html');
+//fonts
+//test
+var Server = require('karma').Server;
+
+/*------------------------------------------------------------------------------------------------*/
+
+/*  JAVASCRIPT TASK */ 
+
+gulp.task('lint', function() {
+  gulp.src(paths.js.dev)
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'),{ verbose: true })
+  .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('devScripts', function() {
+  return gulp.src(paths.js.dev)
+  .pipe(concat('dev.js'))
+  //.pipe(uglify())
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('vendorScripts', function() {
+  return gulp.src(paths.js.vendor)
+  .pipe(concat('vendor.js'))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('concatJs', function() {
+  return gulp.src(['dist/vendor.js', 'dist/dev.js'])
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('cleanConcatJs', function() {
+  gulp.src(['dist/vendor.js', 'dist/dev.js'])
+  .pipe(clean({force: true}));
+});
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+/*  CSS TASK */ 
+
+gulp.task('vendorCss', function () {
+  return gulp.src(paths.css.vendor)
+  .pipe(autoprefixer({
+    browsers: ['last 5 versions'],
+    cascade: true
+  }))
+  .pipe(concat('vendor.css'))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('devCss', function () {
+  return gulp.src(paths.css.dev)
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 5 versions'],
+    cascade: true
+  }))
+  .pipe(concat('dev.css'))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('concatCss', function() {
+  return gulp.src(['dist/vendor.css', 'dist/dev.css'])
+  .pipe(concat('main.css'))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('cleanConcatCss', function() {
+  gulp.src(['dist/vendor.css', 'dist/dev.css'])
+  .pipe(clean({force: true}));
+});
+
+/*------------------------------------------------------------------------------------------------*/
+
+/*  HTML TASK */
+
+gulp.task('moveDirectives', function () {
+  return gulp.src(paths.html.directives)
+  .pipe(rename(function (path) {  
+    path.dirname = "";
+    path.basename = "directive." + path.basename;
+  }))
+  .pipe(minifyHTML({ empty: true }))
+  .pipe(gulp.dest('dist/miame/'));
+});
+
+gulp.task('moveViews', function () {
+  return gulp.src(paths.html.views)
+  .pipe(rename(function (path) {  
+    path.dirname = "";
+    path.basename = "view." + path.basename;
+  }))
+  .pipe(minifyHTML({ empty: true }))
+  .pipe(gulp.dest('dist/miame/'));
+});
+
+/*------------------------------------------------------------------------------------------------*/
+
+/*  TEST TASK */
+
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/test.js',
+    singleRun: true
+  }, done).start();
+});
+
+/*------------------------------------------------------------------------------------------------*/
+
+/*  FONTS TASK */
+
+gulp.task('moveFonts', function () {
+  return gulp.src(paths.fonts.fonts)
+  .pipe(gulp.dest('dist/fonts/'));
+});
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+/*  MORE TASK */
+
+gulp.task('default', function() {
+  runSequence(
+    ['lint','vendorScripts','devScripts', 'vendorCss', 'devCss', 'moveDirectives', 'moveViews', 'moveFonts'],
+    ['concatJs', 'concatCss'],
+    ['cleanConcatJs', 'cleanConcatCss']
+    );
+});
+
+gulp.task('scripts', function() {
+  runSequence(
+    ['lint','vendorScripts','devScripts'],
+    ['concatJs'],
+    ['cleanConcatJs']
+    );
+});
+
+gulp.task('styles', function() {
+  runSequence(
+    ['vendorCss', 'devCss'],
+    ['concatCss'],
+    ['cleanConcatCss']
+    );
+});
+
+gulp.task('watch', function() {
+  var arrayCss = [];
+  arrayCss = arrayCss.concat(paths.css.dev);
+  arrayCss = arrayCss.concat(paths.css.vendor);
+
+  var arrayHtml = [];
+  arrayHtml = arrayHtml.concat(paths.html.directives);
+  arrayHtml = arrayHtml.concat(paths.html.views);
+
+  gulp.watch(paths.js.dev, ['scripts']);
+  gulp.watch(arrayCss, ['styles']);
+  gulp.watch(arrayHtml, ['moveDirectives', 'moveViews']);
+});
---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+app = express();
+
+var messages = [];
+
+app.set('port', (process.env.PORT || 1616));
+
+app.use(express.static(__dirname + '/dist/'));;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/dist/index.html');
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
