87a67d0300858b51162178b37205bb1f9f1bab60
kefala <martin.g.lobo@gmail.com>
2016-05-20 18:11:52 -0300

    Add task

--- src/Home/Collections/a.js
+++ src/Home/Collections/a.js
@@ -1,9 +1,9 @@
+/*
 var i = 0, sum = 0;
 
 while (i++ < 333)
 	sum += (3*i) + ( ((5*i) % 3 !== 0 && i < 200) * (5*i) );
 
-/****************/
 function getMultiples(i) {
 	return (3*i) + ( ((5*i) % 3 !== 0 && i < 200) * (5*i) );
 }
@@ -18,63 +18,6 @@ multiplesOf3And5();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function fnFib(cont = 4) {
-	if (cont === 0 || cont === 1) return 1;
-	else if (cont === 2) return 2;
-		else return (cont - 1) + (cont - 2);
-}
-function fib(i = 1, sum = 0) {
-	if (i > 2000) return sum; 
-	else return fib(i+1, sum += fnFib(i));
-}
-
-
-
-
-
-
-
-//var a = 1, e = 0;
-/*
-while (a > 4000) {
-	e += fib(10000 * a);
-}
-*/
 
 
 function fibo(n = 0) {
@@ -96,19 +39,5 @@ function fn() {
 fn();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
